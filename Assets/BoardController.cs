fe7c4b01fabf788d23cbec3d96e7df6afdd155a7
basdidon <39435008+basdidon@users.noreply.github.com>
2024-03-25 19:21:14 +0700

    -

--- Assets/BoardController.cs
+++ Assets/BoardController.cs
@@ -2,55 +2,107 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+public enum BoardObjectTypes
+{
+    Input,
+    Output,
+    Convoyer
+}
+
+[Serializable]
+public class BoardObjectTypeDataDictionary : UnitySerializedDictionary<BoardObjectTypes, BoardObjectData> { }
 
 [RequireComponent(typeof(Grid))]
 public sealed class BoardController : MonoBehaviour
 {
-    [Header("Debug")]
-    public BoardObjectData input;
-    public BoardObjectData convoyer;
+    public GameObject boardObjectPrefab;
 
-    [SerializeField] Grid grid;
+    public Grid Grid { get; private set; }
+
     [field:SerializeField] List<BoardObject> BoardObjects { get; set; }
-    
+    [SerializeField] Dictionary<BoardObjectTypes, BoardObjectData> BoardObjectDataDictionary;
+    [SerializeField] BoardObjectTypeDataDictionary Dict;
+
     private void Awake()
     {
-        grid = GetComponent<Grid>();
+        Grid = GetComponent<Grid>();
 
         BoardObjects = new();
+        BoardObjectDataDictionary = new();
 
-        CreateBoardObject(input, Vector3Int.zero);
-        CreateBoardObject(convoyer, new Vector3Int(1,0,0));
-        CreateBoardObject(convoyer, new Vector3Int(2,0,0));
+        foreach(var boardTypeName in Enum.GetNames(typeof(BoardObjectTypes)))
+        {
+            var path = $"{boardTypeName}";
+            BoardObjectData boardObjectData = AssetDatabase.LoadAssetAtPath<BoardObjectData>(path);
+        }
+    }
+
+    public bool CreateBoardObject(BoardObjectTypes moduleType, Vector3Int position)
+    {
+        throw new System.NotImplementedException();
     }
 
     public bool CreateBoardObject(BoardObjectData data,Vector3Int position)
     {
-        BoardObject newBoardObject = new(data,position);
-        bool isOverlap = BoardObjects.Any(boardObject => boardObject.Cells.Any(cell => newBoardObject.Cells.Contains(cell)));
+        bool isOverlap = BoardObjects.Any(boardObject => boardObject.Cells.Any(cell => data.Cells.Select(cell => cell + position).Contains(cell)));
 
         if (isOverlap)
             return false;
 
-        BoardObjects.Add(newBoardObject);
-
+        var clone = Instantiate(boardObjectPrefab, transform);
+        BoardObject boardObject = clone.GetComponent<BoardObject>();
+        boardObject.Initialize(this,data,position);
+        BoardObjects.Add(boardObject);
         return true;
     }
 
-    [Serializable]
-    private class BoardObject
-    {
-        [field: SerializeField] public Vector3Int Position { get; set; }
-        public IEnumerable<Vector3Int> Cells => BoardObjectData.Cells.Select(cell => cell + Position);
-
-        public BoardObjectData BoardObjectData { get; }
-
-        public BoardObject(BoardObjectData data, Vector3Int position)
-        {
-            BoardObjectData = data;
-            Position = position;
-        }
-    }
 }
 
+[CustomEditor(typeof(BoardController))]
+public class BoardControllerEditor : Editor 
+{
+    public override VisualElement CreateInspectorGUI()
+    {
+        VisualElement container = new(); 
 
+        InspectorElement.FillDefaultInspector(container, serializedObject, this);
+
+        ObjectField boardObjectDataField = new() { objectType = typeof(BoardObjectData)};
+        container.Add(boardObjectDataField);
+
+        EnumField boardObjectTypeSelector = new(BoardObjectTypes.Convoyer);
+        container.Add(boardObjectTypeSelector);
+
+        Vector3IntField positionField = new();
+        container.Add(positionField);
+
+        Button createBoardObjectBtn = new() { text = "Create boardObject" };
+        container.Add(createBoardObjectBtn);
+
+        createBoardObjectBtn.clicked += () =>
+        {
+            if(boardObjectDataField.value is BoardObjectData objectData)
+            {
+                if((target as BoardController).CreateBoardObject(objectData, positionField.value))
+                {
+                    Debug.Log("created boardObject");
+                }
+                else
+                {
+                    Debug.Log("look like boardObject overlap with the other.");
+                }
+                
+            }
+
+                Debug.Log("boardObjectData is null");
+        };
+
+        return container;
+    }
+}
---
+++ Assets/BoardObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[RequireComponent(typeof(SpriteRenderer))]
+public class BoardObject : MonoBehaviour
+{
+    Vector3Int gridPosition;
+    public Vector3Int GridPosition
+    {
+        get => gridPosition;
+        set
+        {
+            gridPosition = value;
+            OnSetGridPosition();
+        }
+    }
+    public IEnumerable<Vector3Int> Cells => BoardObjectData.Cells.Select(cell => cell + GridPosition);
+
+    public BoardController BoardController { get; private set; }
+    public SpriteRenderer SpriteRenderer { get; private set; }
+    public BoardObjectData BoardObjectData { get; private set; }
+
+    public void Initialize(BoardController boardController,BoardObjectData data,Vector3Int gridPosition)
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        BoardController = boardController;
+        BoardObjectData = data;
+        GridPosition = gridPosition;
+
+        SpriteRenderer.sprite = BoardObjectData.Sprite;
+    }
+
+    void OnSetGridPosition()
+    {
+        transform.position = BoardController.Grid.GetCellCenterWorld(GridPosition);
+    }
+}
